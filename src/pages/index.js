ac6dd9bec2486a0d2bf7489d46a4710749834186
squteronsquter <squter@squter.pl>
2021-09-17 17:50:12 +0200

    Some pages added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,24 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello World!</div>
+  return (
+    <>
+      <h1>Moje obrazy</h1>
+      <ul>
+        <li>
+          <Link to="/about">O nas</Link>
+        </li>
+        <li>
+          <Link to="/contact">Kontakt</Link>
+        </li>
+        <li>
+          <Link to="/paintings/">Obrazy</Link>
+        </li>
+        <li>
+          <Link to="/paintings/history">Historia</Link>
+        </li>
+      </ul>
+    </>
+  )
 }
